ffa1d3dfad009e6492d5b8d16573f98f44ec87d6
bhsiao55 <bhsiao55@gmail.com>
2014-06-21 14:02:46 -0700

    fix space issue

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 	// set up ========================
 	var express  = require('express');
-	var app      = express(); 								// create our app w/ express
-	var mongoose = require('mongoose'); 					// mongoose for mongodb
+	var app      = express();                        // create our app w/ express
+	var mongoose = require('mongoose');              // mongoose for mongodb
 
 	// configuration =================
     mongoose.connect('mongodb://todo:todo@localhost/todo'); 	// connect to mongoDB database on modulus.io
 
 	app.configure(function() {
-		app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-		app.use(express.logger('dev')); 						// log every request to the console
-		app.use(express.json());								// pull information from html in POST
+		app.use(express.static(__dirname + '/public'));         // set the static files location /public/img will be /img for users
+		app.use(express.logger('dev'));                         // log every request to the console
+		app.use(express.json());                                // pull information from html in POST
 		app.use(express.urlencoded());
-		//app.use(express.bodyParser()); 						// deprecated way to pull html from POST	
-		app.use(express.methodOverride()); 						// simulate DELETE and PUT
+		//app.use(express.bodyParser());                        // deprecated way to pull html from POST	
+		app.use(express.methodOverride());                      // simulate DELETE and PUT
 	});
 	var todoSchema = mongoose.Schema({
     text : String,
